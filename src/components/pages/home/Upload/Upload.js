dbc8ee25ba6d17a5b52996708bfb32560576219b
Etnik99 <etnik.krivaqa99@gmail.com>
2023-07-26 15:56:19 +0200

    Lnading page

---
+++ src/components/pages/home/Upload/Upload.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Upload = () => {
+  return (
+    <div>
+        <h1>Outkos</h1>
+        <h1>Outkos</h1>
+        <h1>Outkos</h1>
+        <h1>Outkos</h1>
+        <h1>Outkos</h1>
+        <h1>Outkos</h1>
+        <h1>Outkos</h1>
+        <h1>Outkos</h1>
+        <h1>Outkos</h1>
+        <h1>Outkos</h1>
+        <h1>Outkos</h1>
+        <h1>Outkos</h1>
+        <h1>Outkos</h1>
+    </div>
+  )
+}
+
+export default Upload
